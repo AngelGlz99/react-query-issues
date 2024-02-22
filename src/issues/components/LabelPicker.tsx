2b352cea72d5824bc86a4f6a2138d01491440aec
Angel Gonzalez <angelglz0899@gmail.com>
2024-02-21 17:54:17 -0700

    Update clase LabelIssues

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,31 @@
-
+import { useQuery } from "@tanstack/react-query";
 export const LabelPicker = () => {
+
+  const getLabels = async () => {
+    const res = await fetch('https://api.github.com/repos/facebook/react/labels')
+    const data = await res.json()
+    console.log(data)
+    return data
+  }
+
+  const labelsQuery = useQuery(
+    {
+      queryKey: ['labels'],
+      queryFn: getLabels
+    }
+  )
+
+
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      >
+        Primary
+      </span>
+
     </div>
   )
 }
